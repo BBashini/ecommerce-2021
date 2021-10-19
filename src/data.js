5f57b7da72a2c8f6d9e85586964e6e3dc7d468aa
BBashini <b4balach@ryerson.ca>
2021-10-19 01:13:18 -0400

    changes to images

--- src/data.js
+++ src/data.js
@@ -8,14 +8,14 @@ export const sliderItems = [
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
+      img: "https://i.ibb.co/8msBd6v/brideandgroom-optimized.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://i.ibb.co/cXFnLLV/3.png",
+      img: "https://i.ibb.co/RYRcLhX/witch-And-Bride.png",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
